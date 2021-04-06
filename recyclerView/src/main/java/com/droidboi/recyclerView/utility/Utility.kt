526d080fceea507a8a91962bd76536246c89ff3d
ritwikjamuar <ritwikjamuar@gmail.com>
2021-04-06 11:28:51 +0530

    Added a Type-Alias in Utility.kt to notify an event from RecyclerView.ViewHolder to RecyclerView.Adapter.

--- recyclerView/src/main/java/com/droidboi/recyclerView/utility/Utility.kt
+++ recyclerView/src/main/java/com/droidboi/recyclerView/utility/Utility.kt
@@ -1,1 +1,11 @@
 package com.droidboi.recyclerView.utility
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Type-Alias as a General Purpose event notifier from [RecyclerView.ViewHolder]
+ * to the [RecyclerView.Adapter].
+ *
+ * @author Ritwik Jamuar
+ */
+typealias ViewHolderListener = (Int) -> Unit
