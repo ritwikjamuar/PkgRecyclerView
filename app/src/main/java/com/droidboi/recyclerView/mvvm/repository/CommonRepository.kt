014db0efbcbaf4905c61402b6ce1554e2264b45e
ritwikjamuar <ritwikjamuar@gmail.com>
2021-04-07 14:29:32 +0530

    Added CommonRepository.kt as a Repository Class that provides the data.

---
+++ app/src/main/java/com/droidboi/recyclerView/mvvm/repository/CommonRepository.kt
@@ -0,0 +1,8 @@
+package com.droidboi.recyclerView.mvvm.repository
+
+/**
+ * A Repository that provides all the necessary data to be shown in the UI.
+ *
+ * @author Ritwik Jamuar
+ */
+class CommonRepository
