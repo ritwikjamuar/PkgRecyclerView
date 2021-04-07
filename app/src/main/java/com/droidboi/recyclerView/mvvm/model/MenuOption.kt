8e3aa9ccf7ef12b06d6baf4ac1c5cba34b29a06d
ritwikjamuar <ritwikjamuar@gmail.com>
2021-04-07 14:26:46 +0530

    Created a Data Class MenuOption.kt that represents an individual Menu Option.

---
+++ app/src/main/java/com/droidboi/recyclerView/mvvm/model/MenuOption.kt
@@ -0,0 +1,13 @@
+package com.droidboi.recyclerView.mvvm.model
+
+/**
+ * Data Class to hold information of a particular Menu Item.
+ *
+ * @param id [Int] denoting the ID of this Menu Item.
+ * @param name [Int] denoting the Name of this Menu Item.
+ * @author Ritwik Jamuar
+ */
+data class MenuOption(
+	val id: Int,
+	val name: String
+)
