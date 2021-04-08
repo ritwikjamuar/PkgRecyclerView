9ccf67bba90bfffc23951a96f2dc60f54d951501
ritwikjamuar <ritwikjamuar@gmail.com>
2021-04-08 15:00:58 +0530

    Added code in Utility.kt to help navigate user to another Activity.

--- app/src/main/java/com/droidboi/recyclerView/utility/Utiltity.kt
+++ app/src/main/java/com/droidboi/recyclerView/utility/Utiltity.kt
@@ -1,1 +1,15 @@
 package com.droidboi.recyclerView.utility
+
+import android.app.Activity
+
+import android.content.Intent
+
+/**
+ * Navigates any [Activity] to another [Activity].
+ *
+ * @param className [Class] of type [Activity] as the name of the [Activity] Class
+ *   we want to navigate.
+ */
+fun Activity.navigate(className: Class<out Activity>) {
+	startActivity(Intent(this, className))
+}
