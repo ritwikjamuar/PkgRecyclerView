9b5f2254c7d43c599a84c29c2e5cc01552bdeba8
ritwikjamuar <ritwikjamuar@gmail.com>
2021-04-09 17:31:55 +0530

    Added Demonstration2Activity.kt with it's Layout XML activity_demonstration2.xml.

---
+++ app/src/main/java/com/droidboi/recyclerView/ui/activity/Demonstration2Activity.kt
@@ -0,0 +1,16 @@
+package com.droidboi.recyclerView.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+import com.droidboi.recyclerView.R
+
+class Demonstration2Activity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_demonstration2)
+	}
+
+}
