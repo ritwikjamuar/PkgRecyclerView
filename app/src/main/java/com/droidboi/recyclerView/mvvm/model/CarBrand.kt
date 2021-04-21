44dabfea2cbe9cf8854a467e89f2de69cddc0dcd
ritwikjamuar <ritwikjamuar@gmail.com>
2021-04-21 17:19:45 +0530

    Removed the usage of @field: Json() in the Data Members of the Response, as they were not working properly.

--- app/src/main/java/com/droidboi/recyclerView/mvvm/model/CarBrand.kt
+++ app/src/main/java/com/droidboi/recyclerView/mvvm/model/CarBrand.kt
@@ -1,25 +1,23 @@
 package com.droidboi.recyclerView.mvvm.model
 
-import com.squareup.moshi.Json
-
 /**
  * Data Class to hold information of a particular Car Brand.
  *
  * @param id [Int] denoting ID.
  * @param name [String] denoting the name of this Car Brand.
  * @param founded [Int] denoting the Year this Car Brand formed.
- * @param brandImageURL [String] as the URL of the Car Brand Image.
- * @param headQuarters [String] denoting the Head Quarter of this Car Brand.
+ * @param brand_image_url [String] as the URL of the Car Brand Image.
+ * @param head_quarters [String] denoting the Head Quarter of this Car Brand.
  * @param founders [List] of [String] containing the name of the Founders of this Car Brand.
  * @param popularCars [List] of [String] containing the List of Popular Cars under this Car Brand.
  * @author Ritwik Jamuar
  */
 data class CarBrand(
-	@field: Json(name = "id") val id: Int,
-	@field: Json(name = "name") val name: String,
-	@field: Json(name = "founded") val founded: Int,
-	@field: Json(name = "brand_image_url") val brandImageURL: String,
-	@field: Json(name = "head_quarters") val headQuarters: String,
-	@field: Json(name = "founders") val founders: List<String>,
-	@field: Json(name = "popularCars") val popularCars: List<String>
+	val id: Int,
+	val name: String,
+	val founded: Int,
+	val brand_image_url: String,
+	val head_quarters: String,
+	val founders: List<String>,
+	val popularCars: List<String>
 )
--- app/src/main/java/com/droidboi/recyclerView/mvvm/network/CarBrandsREST.kt
+++ app/src/main/java/com/droidboi/recyclerView/mvvm/network/CarBrandsREST.kt
@@ -2,15 +2,13 @@ package com.droidboi.recyclerView.mvvm.network
 
 import com.droidboi.recyclerView.mvvm.model.CarBrand
 
-import com.squareup.moshi.Json
-
 data class CarBrandsResponse(
-	@field: Json(name = "status") val status: String,
-	@field: Json(name = "result") val result: CarBrandsResult
+	val status: String,
+	val result: CarBrandsResult
 )
 
 data class CarBrandsResult(
-	@field: Json(name = "brands") val brands: List<CarBrand>,
-	@field: Json(name = "page_number") val pageNumber: Int,
-	@field: Json(name = "total_pages") val totalPages: Int
+	val brands: List<CarBrand>,
+	val page_number: Int,
+	val total_pages: Int
 )
